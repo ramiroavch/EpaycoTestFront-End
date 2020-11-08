6b824ed84e1c9ce9dcb417566131b771a56e664c
RAM <ramirovargas.dev@gmail.com>
2020-11-07 23:48:15 -0400

    Feature(app): Agrega el enrutamiento y los estilos de App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React from 'react'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import Navbar from './components/sidebar/Sidebar'
+import Dashboard from './containers/dashboard/Dashboard'
+import AuthPayment from './containers/authPayment/AuthPayment'
+import CheckWallet from './containers/checkWallet/CheckWallet'
+import MakePayment from './containers/makePayment/MakePayment'
+import RechargeWallet from './containers/rechargeWallet/RechargeWallet'
+import RegisterClient from './containers/registerClient/RegisterClient'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+const App = () =>{
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Dashboard}/>
+        <Route exact path="/wallet" component={CheckWallet}/>
+        <Route exact path="/wallet/recharge" component={RechargeWallet}/>
+        <Route exact path="/pay" component={MakePayment}/>
+        <Route exact path="/pay/auth" component={AuthPayment}/>
+        <Route exact path="/register" component={RegisterClient}/>
+        <Route path="*"/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
