3b4203158371bcc10ecee6d165ce0c0147b21584
RAM <ramirovargas.dev@gmail.com>
2020-11-08 03:05:41 -0400

    Feature(registerClient): Agrega manejos de estados y manejo de errores
    manejor de errores de campo vacío

--- src/containers/registerClient/RegisterClient.js
+++ src/containers/registerClient/RegisterClient.js
@@ -1,21 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import clases from './RegisterClient.css'
+import Card from '../../components/card/Card.js'
 
 const RegisterClient = (props)=>{
+    const [myForm,updateForm] = useState({
+        data: {
+            document: {
+                value: '',
+                clicked:false,
+                error:false
+            },
+            name:{
+                value:'',
+                clicked:false,
+                error:false
+            },
+            lastname:{
+                value:'',
+                clicked:false,
+                error:false
+            },
+            email:{
+                value:'',
+                clicked:false,
+                error:false
+            },
+            phone:{
+                value:'',
+                clicked:false,
+                error:false
+            }
+        }
+    })
+    const [error,setError]=useState(false);
+    const updateValue= (event,identifier)=>{
+        const origin= {...myForm};
+        updateForm({data:{[identifier]:{value:[event.target.value]}}})
+    }
+    const updateHandler = (event)=>{
+        //event.preventDefault();
+        const emailRegex=/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/; 
+        if (emailRegex.test(event.target.value))
+            setError(false);
+        else 
+            setError(true);
+    }
+    const clicked = event => {
+        updateForm({data:{document:{clicked:true}}});
+        console.log(myForm);
+    }
     return(
-        <div className=''>
+        <Card>
             <form>
                 <div className='form-group'>
                     <label>Documento</label>
-                    <input type='number' className='form-control' placeholder='Documento'/>
+                    <input type='number' className='form-control' placeholder='Documento' onBlur={(event)=>updateValue(event,'documento')}/>
                 </div>
                 <div className='form-group'>
-                    <label>Password</label>
-                    <input type='password' className='form-control' placeholder='Password'/>
+                    <label>Nombre</label>
+                    <input type='string' className='form-control' placeholder='Nombre' onBlur={(event)=>updateValue(event,'name')}/>
                 </div>
-                <button type="submit" className='btn btn-default btn-color'>Registrar</button>
+                <div className='form-group'>
+                    <label>Apellido</label>
+                    <input type='string' className='form-control' placeholder='Apellido' onBlur={(event)=>updateValue(event,'lastname')}/>
+                </div>
+                <div className='form-group'>
+                    <label>Correo</label>
+                    <input type='email' className={error ? 'form-control error-border': 'form-control'} placeholder='Correo' onBlur={(event)=>updateValue(event,'email')}/>
+                </div>
+                <div className='form-group'>
+                    <label>Telefono</label>
+                    <input type='number' className='form-control' placeholder='Telefono' onBlur={(event)=>updateValue(event,'phone')}/>
+                </div>
+                <button type="submit" className='btn btn-default btn-color' disabled={error}>Registrar</button>
             </form>
-        </div>
+        </Card>
     )
 }
 
