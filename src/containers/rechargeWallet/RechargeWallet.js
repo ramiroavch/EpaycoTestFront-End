dedd3b04863677944b373225e7b08754a7fa0587
RAM <ramirovargas.dev@gmail.com>
2020-11-09 14:10:24 -0400

    Feature(rechargeWallet): Agrega consumo de servicio y manejo de errores

--- src/containers/rechargeWallet/RechargeWallet.js
+++ src/containers/rechargeWallet/RechargeWallet.js
@@ -1,6 +1,9 @@
 import React ,{ useState } from 'react'
 import Card from '../../components/card/Card'
 import {validateValue} from '../../commons/Utils'
+import axios from '../../axios';
+import Spinner from '../../components/spinner/Spinner';
+import errorHandler from '../../highorder/errorHandler';
 
 const RechargeWallet = (props)=>{
     const [myForm,updateForm] = useState({
@@ -21,8 +24,67 @@ const RechargeWallet = (props)=>{
                 error:true
             }
         },
-        error:false
+        error:false,
+        ok:false,
+        loading:false
     })
+
+    let actualizarEstado = (data) => {
+        updateForm(Object.assign({}, myForm, data));
+    };
+
+    const initializeState=(ok)=>{
+        updateForm({
+            data: {
+                document: {
+                    value: '',
+                    clicked:false,
+                    error:true
+                },
+                phone:{
+                    value:'',
+                    clicked:false,
+                    error:true
+                },
+                amount:{
+                    value:'',
+                    clicked:false,
+                    error:true
+                }
+            },
+            error:false,
+            ok:ok,
+            loading:false
+        });
+    }
+
+    const recharge =async (event)=>{
+        event.preventDefault();
+        try{
+            actualizarEstado({
+                loading: true
+            })
+            const response= await axios.put('/client/wallet',
+                {
+                    "document":myForm.data.document.value.toString(),
+                    "phone":myForm.data.phone.value.toString(),
+                    "amount": parseFloat(myForm.data.amount.value)
+                }
+            )
+            if(!response.data)
+                throw new Error("Error http");
+            actualizarEstado({
+                ok:true,
+                loading:false
+            })
+            initializeState(true);
+        }
+        catch (err){
+            actualizarEstado({
+                loading:false
+            })
+        }
+    }
 
     const updateValue= (event,identifier,type)=>{
         event.preventDefault();
@@ -55,8 +117,9 @@ const RechargeWallet = (props)=>{
         return error;
     }
     return(
-        <Card>
-            <form>
+        myForm.loading  ? <Spinner/> :
+        <Card title='Recargar Billetera'>
+            <form onSubmit={recharge}>
                 <div className='form-group'>
                     <label>Documento</label>
                     <input type='number' className={getError('document')  ? 'form-control error-border': 'form-control'} placeholder='Documento' onChange={(event)=>updateValue(event,'document','number')}/>
@@ -67,12 +130,13 @@ const RechargeWallet = (props)=>{
                 </div>
                 <div className='form-group'>
                     <label>Monto</label>
-                    <input type='number' className={getError('amount')  ? 'form-control error-border': 'form-control'} placeholder='Monto' onChange={(event)=>updateValue(event,'amount','decimal')}/>
+                    <input type='number' className={getError('amount')  ? 'form-control error-border': 'form-control'} placeholder='Monto' onChange={(event)=>updateValue(event,'amount','decimal')} step=".01"/>
                 </div>
                 <button type="submit" className='btn btn-default btn-color' disabled={!myForm.error} >Recargar</button>
+                {myForm.ok ? <span className='text-success'>Saldo recargado con exito!!</span>:null}
             </form>
         </Card>
     )
 }
 
-export default RechargeWallet
+export default errorHandler(RechargeWallet,axios);
