c461abc0f70b2850f7f8b2e21268550af3eef3c4
RAM <ramirovargas.dev@gmail.com>
2020-11-08 22:25:11 -0400

    Feature(rechargeWallet): Agrega validaciones de formulario

--- src/containers/rechargeWallet/RechargeWallet.js
+++ src/containers/rechargeWallet/RechargeWallet.js
@@ -1,23 +1,75 @@
-import React from 'react'
+import React ,{ useState } from 'react'
 import Card from '../../components/card/Card'
+import {validateValue} from '../../commons/Utils'
 
 const RechargeWallet = (props)=>{
+    const [myForm,updateForm] = useState({
+        data: {
+            document: {
+                value: '',
+                clicked:false,
+                error:true
+            },
+            phone:{
+                value:'',
+                clicked:false,
+                error:true
+            },
+            amount:{
+                value:'',
+                clicked:false,
+                error:true
+            }
+        },
+        error:false
+    })
+
+    const updateValue= (event,identifier,type)=>{
+        event.preventDefault();
+        const parent = {...myForm};
+        const origin= {...parent['data']};
+        const element = {...origin[identifier]}
+        element.value = event.target.value;
+        element.error = validateValue(event.target.value,type);
+        element.clicked = true;
+        origin[identifier]= element;
+        parent['data']=origin;
+        parent.error=activateButton(origin);
+        updateForm (parent);
+    }
+    const getError = (identifier)=>{
+        const origin= {...myForm.data};
+        const element = {...origin[identifier]};
+        return element.error && element.clicked;
+    }
+
+    const activateButton=(data)=>{
+        const origin= data;
+        let error = true;
+        for (let element in origin){
+            if (origin[element].error===true){
+                error =false;
+            }
+                
+        }
+        return error;
+    }
     return(
         <Card>
             <form>
                 <div className='form-group'>
                     <label>Documento</label>
-                    <input type='number' className='form-control' placeholder='Documento'/>
+                    <input type='number' className={getError('document')  ? 'form-control error-border': 'form-control'} placeholder='Documento' onChange={(event)=>updateValue(event,'document','number')}/>
                 </div>
                 <div className='form-group'>
                     <label>Telefono</label>
-                    <input type='number' className='form-control' placeholder='Telefono'/>
+                    <input type='number' className={getError('phone')  ? 'form-control error-border': 'form-control'} placeholder='Telefono' onChange={(event)=>updateValue(event,'phone','number')}/>
                 </div>
                 <div className='form-group'>
                     <label>Monto</label>
-                    <input type='number' className='form-control' placeholder='Monto'/>
+                    <input type='number' className={getError('amount')  ? 'form-control error-border': 'form-control'} placeholder='Monto' onChange={(event)=>updateValue(event,'amount','decimal')}/>
                 </div>
-                <button type="submit" className='btn btn-default btn-color'>Recargar</button>
+                <button type="submit" className='btn btn-default btn-color' disabled={!myForm.error} >Recargar</button>
             </form>
         </Card>
     )
