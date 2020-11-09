d0e3c472846923db4c60a08b5a224401c93e0671
RAM <ramirovargas.dev@gmail.com>
2020-11-09 14:12:52 -0400

    Feature(app): Agrega titulo para los contenedores de las funcionalidades

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,6 +4,7 @@ const Card=(props)=>(
     <div className='col-lg-6 col-md-6 col-sm-8 col-12 register-form mt-5 ml-auto mr-auto'>
         <div className='card'>
             <div className='card-body'>
+                {props.title ? <h5 class="card-title font-weight-bold text-info">{props.title}</h5>:null}
                 {props.children}
             </div>
         </div>
