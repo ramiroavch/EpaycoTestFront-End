5594a2ddfc87939b2cae2882767c40a50c3eec25
RAM <ramirovargas.dev@gmail.com>
2020-11-10 04:38:12 -0400

    Fix(registerClient): Arregla reinicio de estado
    Agrega spinner tambien

--- src/containers/registerClient/RegisterClient.js
+++ src/containers/registerClient/RegisterClient.js
@@ -36,13 +36,48 @@ const RegisterClient = (props)=>{
             }
         },
         error:false,
-        ok:false
+        ok:false,
+        loading:false
     })
 
     let actualizarEstado = (data) => {
         updateForm(Object.assign({}, myForm, data));
     };
 
+    const initializeState=(ok)=>{
+        updateForm({
+            data: {
+                document: {
+                    value: '',
+                    clicked:false,
+                    error:true
+                },
+                name:{
+                    value:'',
+                    clicked:false,
+                    error:true
+                },
+                lastname:{
+                    value:'',
+                    clicked:false,
+                    error:true
+                },
+                email:{
+                    value:'',
+                    clicked:false,
+                    error:true
+                },
+                phone:{
+                    value:'',
+                    clicked:false,
+                    error:true
+                }
+            },
+            error:false,
+            ok:ok,
+            loading:false
+        });
+    }
     const register =async (event)=>{
         event.preventDefault();
         try{
@@ -64,11 +99,10 @@ const RegisterClient = (props)=>{
                 ok: true,
                 loading:false
             })
+            initializeState(true);
         }
         catch (err){
-            actualizarEstado({
-                loading:false
-            })
+            initializeState(false);
         }
     }
 
@@ -103,33 +137,37 @@ const RegisterClient = (props)=>{
         }
         return error;
     }
-    return(
-        <Card title='Registrar Usuario'>
-            <form onSubmit= {register}>
-                <div className='form-group'>
-                    <label>Documento</label>
-                    <input type='number' className={getError('document')  ? 'form-control error-border': 'form-control'} placeholder='Documento' onChange={(event)=>updateValue(event,'document','string')}/>
-                </div>
-                <div className='form-group'>
-                    <label>Nombre</label>
-                    <input type='string' className={getError('name') ? 'form-control error-border': 'form-control'} placeholder='Nombre' onChange={(event)=>updateValue(event,'name','string')}/>
-                </div>
-                <div className='form-group'>
-                    <label>Apellido</label>
-                    <input type='string' className={getError('lastname') ? 'form-control error-border': 'form-control'} placeholder='Apellido' onChange={(event)=>updateValue(event,'lastname','string')}/>
-                </div>
-                <div className='form-group'>
-                    <label>Correo</label>
-                    <input type='email' className={getError('email') ? 'form-control error-border': 'form-control'} placeholder='Correo' onChange={(event)=>updateValue(event,'email','email')}/>
-                </div>
-                <div className='form-group'>
-                    <label>Telefono</label>
-                    <input type='number' className={getError('phone') ? 'form-control error-border': 'form-control'} placeholder='Telefono' onChange={(event)=>updateValue(event,'phone','number')}/>
-                </div>
-                <button type="submit" className='btn btn-default btn-color' disabled={!myForm.error}>Registrar</button>
-                {myForm.ok ? <span className='text-success'>Se registro el cliente con exito!</span>:null}
-            </form>
-        </Card>
+    return( 
+        myForm.loading
+        ?
+        <Spinner/>:
+            <Card title='Registrar Usuario'>
+                <form onSubmit= {register}>
+                    <div className='form-group'>
+                        <label>Documento</label>
+                        <input type='number' className={getError('document')  ? 'form-control error-border': 'form-control'} placeholder='Documento' onChange={(event)=>updateValue(event,'document','string')}/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Nombre</label>
+                        <input type='string' className={getError('name') ? 'form-control error-border': 'form-control'} placeholder='Nombre' onChange={(event)=>updateValue(event,'name','string')}/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Apellido</label>
+                        <input type='string' className={getError('lastname') ? 'form-control error-border': 'form-control'} placeholder='Apellido' onChange={(event)=>updateValue(event,'lastname','string')}/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Correo</label>
+                        <input type='email' className={getError('email') ? 'form-control error-border': 'form-control'} placeholder='Correo' onChange={(event)=>updateValue(event,'email','email')}/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Telefono</label>
+                        <input type='number' className={getError('phone') ? 'form-control error-border': 'form-control'} placeholder='Telefono' onChange={(event)=>updateValue(event,'phone','number')}/>
+                    </div>
+                    <button type="submit" className='btn btn-default btn-color' disabled={!myForm.error}>Registrar</button>
+                    {myForm.ok ? <span className='text-success'>Se registro el cliente con exito!</span>:null}
+                </form>
+    </Card> 
+
     )
 }
 
